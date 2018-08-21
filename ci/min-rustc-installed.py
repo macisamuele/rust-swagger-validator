746b8edf26a394618799585c92df504cb7a548e1
Samuele Maci <macisamuele@gmail.com>
2018-08-21 14:08:50 +0200

    Add tool to detect min rustc version installed

---
+++ ci/min-rustc-installed.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import subprocess
+import sys
+
+from pkg_resources import parse_version
+
+
+MIN_VERSION = '1.30.0-nightly'
+
+
+def run_command(command):
+    print('Run command: {command}'.format(command=command), file=sys.stderr)
+    return_code, output = None, None
+    try:
+        return_code, output = 0, subprocess.check_output(
+            command,
+            stderr=subprocess.STDOUT,
+            shell=True,
+        ).decode('utf-8')
+    except subprocess.CalledProcessError as e:
+        return_code, output = e.returncode, e.output.decode('utf-8')
+    print('[return_code={return_code}] | {output}'.format(return_code=return_code, output=output), file=sys.stderr)
+    return return_code, output
+
+
+return_code, rustc_version_output = run_command('rustc --version --verbose')
+assert return_code == 0, 'rustc --version --verbose failed'
+
+
+# `rustc --version --verbose` output looks like the following
+# rustc 1.30.0-nightly (73c78734b 2018-08-05)
+# binary: rustc
+# commit-hash: 73c78734bae8f2947a4bfdeabebeeb84ccf0b0e1
+# commit-date: 2018-08-05
+# host: x86_64-apple-darwin
+# release: 1.30.0-nightly
+# LLVM version: 7.0
+rustc_version = parse_version([
+    line.replace('release: ', '')
+    for line in rustc_version_output.splitlines()
+    if line.startswith('release:')
+][0])
+
+
+assert parse_version(MIN_VERSION) <= rustc_version, 'Min rustc requirement is not satisfied, please update cargo and rustc'
