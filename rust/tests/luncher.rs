0fd2b46866efe68bf7224f7db46af1443d84c8ce
Samuele Maci <macisamuele@gmail.com>
2018-08-27 20:45:57 +0200

    Move tests into dedicated testing directory

---
+++ rust/tests/luncher.rs
@@ -0,0 +1,15 @@
+#![deny(
+    anonymous_parameters,
+    bad_style,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_results,
+    unused_qualifications,
+)]
+
+extern crate rust_swagger_validator;
+
+mod test_lib;
+mod test_loaders;
