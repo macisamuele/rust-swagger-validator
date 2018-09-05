2d0e3af3a674739998af8121844eaa141854d42a
Samuele Maci <macisamuele@gmail.com>
2018-09-05 21:29:02 +0200

    Add tool to start debug build on travis

---
+++ ci/debug-travis-build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -euo pipefail -o posix -o functrace
+
+SCRIPT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+pushd "${SCRIPT_DIR}"
+
+if [ $# -ne 1 ]; then
+    echo -e "Usage: $0 <travis build id>\nMore Information on https://docs.travis-ci.com/user/running-build-in-debug-mode/" > /dev/stderr
+    exit 1
+fi
+travisBuildId="$1"
+
+if [ -f travis_token.txt ]; then
+    travisToken="$(cat travis_token.txt)"
+else
+    echo "
+You can avoid writing the token all the times by writing it once in
+${SCRIPT_DIR}/travis_token.txt
+(be aware that the file should not be  committed on the repository)
+" > /dev/stderr
+    read -s -p "Introduce travis token
+    NOTE: no characters will be visible while typing to protect your password length
+" travisToken
+fi
+
+curl \
+    --request POST \
+    --header "Content-Type: application/json" \
+    --header "Accept: application/json" \
+    --header "Travis-API-Version: 3" \
+    --header "Authorization: token ${travisToken}" \
+    --data "{\"quiet\": true}" \
+    https://api.travis-ci.com/job/${travisBuildId}/debug
+
+popd
