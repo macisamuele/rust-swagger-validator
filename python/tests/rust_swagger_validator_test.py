2937f2435c3ae0a2ba25b7137c16b94425b8d570
Samuele Maci <macisamuele@gmail.com>
2018-09-10 21:29:15 +0200

    pre-commit run --all-files

--- python/tests/rust_swagger_validator_test.py
+++ python/tests/rust_swagger_validator_test.py
@@ -1,2 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
 def test_is_module_importable():
     import rust_swagger_validator  # noqa
